34ebd9a853e61aad8245e8e1ad55973f97ccac0f
Ubaydullo <ubaydullo.mustafaev@mail.ru>
2022-08-11 14:57:55 +0500

    Testing...

--- src/components/Element/index.jsx
+++ src/components/Element/index.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGlobe,
-  faLocationDot,
-} from "@fortawesome/free-solid-svg-icons";
+import { faGlobe, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
